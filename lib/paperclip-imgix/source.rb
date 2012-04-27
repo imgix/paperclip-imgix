03fed4b05455c9ae6d952d0e8d8da04e5c60f6a9
Jeremy Larkin <jeremy@zebrafishlabs.com>
2012-04-27 13:16:49 -0400

    Added stub source object

---
+++ lib/paperclip-imgix/source.rb
@@ -0,0 +1,33 @@
+require 'digest/md5'
+
+module Paperclip::Imgix
+  class Source
+    attr_reader :name, :type
+
+    def initialize(options)
+      options = options.symbolize_keys
+      @name = options[:name]
+      @type = options[:type]
+      @signature_token = options[:signature_token]
+
+      case @type
+      when :web_folder
+        @base_url = options[:base_url]
+      else
+        raise "Invalid source type"
+      end
+    end
+
+    def url(attachment, style, options={})
+      orig_url = attachment.url(:original, :escape => true, :timestamp => false)
+      query = style.query(@attachment, options)
+      path = "#{orig_url.sub('http://s3.amazonaws.com/paperclip-imgix', '')}?#{query}"
+      if @signature_token
+        signature = Digest::MD5.hexdigest(@signature_token + path)
+        path << "&s=" << signature
+      end
+      "http://paperclip.imgix.net#{path}"
+    end
+
+  end
+end
